a145d08c08ed0089c9df00a62f537634ab10e83e
suryajidesai <suryajidesai5138@gmail.com>
2022-12-24 17:47:11 +0530

    q

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "myJune2021Key" 
+    default     =  "devops" 
 }
 
 variable "instance_type" { 
@@ -39,7 +39,7 @@ variable "acl" {
 variable "bucket_prefix" {
     type        = string
     description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "my-s3bucket-"
+    default     = "project-surya"
 }
 variable "tags" {
     type        = map
